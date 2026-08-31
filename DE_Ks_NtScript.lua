40818aa9ff08ff57486c9dbd56cb893c8f2535ba
NtScripts <mrwillinggl@gmail.com>
2026-08-31 00:14:50 -0600

    Update and rename AutDelByNt.lua to DE_Ks_NtScript.lua

---
+++ DE_Ks_NtScript.lua
@@ -0,0 +1,6 @@
+-- Última actualización: 30/08/2026
+-- ========================================
+
+local scriptUrl = "https://api.pandauth.com/vss/tu-id-de-script-aqui"
+
+loadstring(game:HttpGet(scriptUrl))()
