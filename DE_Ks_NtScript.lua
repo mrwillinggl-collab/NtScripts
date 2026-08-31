6ff46efe0ac0bf2570dacb7b579b1944f0e5d3be
NtScripts <mrwillinggl@gmail.com>
2026-08-31 01:22:20 -0600

    Remove unnecessary local variable declaration

--- DE_Ks_NtScript.lua
+++ DE_Ks_NtScript.lua
@@ -1,6 +1,1 @@
--- Última actualización: 30/08/2026
--- ========================================
-
-local scriptUrl = "https://vss.pandauth.com/kv/22654009558848f8"
-
-loadstring(game:HttpGet(scriptUrl))()
+loadstring(game:HttpGet("https://vss.pandauth.com/kv/22654009558848f8"))()
